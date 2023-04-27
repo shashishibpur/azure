f8bcb8d17bb9a8a4a0325c7ab57a2723112affc9
shashishibpur <67359200+shashishibpur@users.noreply.github.com>
2023-04-27 18:51:35 +0530

    Update AzureDemoApplication.java

--- src/main/java/com/example/azureDemo/AzureDemoApplication.java
+++ src/main/java/com/example/azureDemo/AzureDemoApplication.java
@@ -3,7 +3,6 @@ package com.example.azureDemo;
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.identity.implementation.IdentityClientOptions;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
